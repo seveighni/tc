2d1fa2d81ceb86ab9a353e2d27c1f1e1dae2063f
Seveighni <seveighni@proton.me>
2023-12-30 10:01:51 +0200

    Add project template

---
+++ src/test/java/com/seveighni/transportcompany/TransportcompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seveighni.transportcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportcompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
